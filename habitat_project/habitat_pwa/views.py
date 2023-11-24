af9200aea3e12817e7737c5cba57c6a65975a4b0
Matthew Kriel <matthew.k@skrmiish.com>
2023-11-24 13:47:28 +0200

    getting there baby

--- habitat_project/habitat_pwa/views.py
+++ habitat_project/habitat_pwa/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-
-# Create your views here.
-
-from django.http import HttpResponse
+from .models import ArtistLineup
 
 
 def base_layout(request):
     template = 'habitat_pwa/base.html'
-    return render(request, template)
+
+    lineup_list = ArtistLineup.objects.filter(day_of_week=0)
+    print(lineup_list)
+    context = {
+        "lineup_list": lineup_list
+    }
+
+    return render(request, template, context)
